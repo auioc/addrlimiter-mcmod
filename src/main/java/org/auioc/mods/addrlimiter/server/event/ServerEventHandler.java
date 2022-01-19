c120ee1747a522bd268d79c615e035a30dc9accc
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-19 13:42:17 +0800

    feat: handle server events

--- src/main/java/org/auioc/mods/addrlimiter/server/event/ServerEventHandler.java
+++ src/main/java/org/auioc/mods/addrlimiter/server/event/ServerEventHandler.java
@@ -1,7 +1,12 @@
 package org.auioc.mods.addrlimiter.server.event;
 
+import org.auioc.mods.addrlimiter.server.address.AddressHandler;
 import org.auioc.mods.addrlimiter.server.command.ServerCommandRegistry;
+import org.auioc.mods.arnicalib.server.event.impl.ServerLoginEvent;
+import net.minecraft.network.ConnectionProtocol;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public final class ServerEventHandler {
@@ -9,6 +14,25 @@ public final class ServerEventHandler {
     @SubscribeEvent
     public static void registerCommands(final RegisterCommandsEvent event) {
         ServerCommandRegistry.register(event.getDispatcher());
+    }
+
+
+    @SubscribeEvent
+    public static void onPlayerAttemptLogin(final ServerLoginEvent event) {
+        if (event.getPacket().getIntention() == ConnectionProtocol.LOGIN) {
+            AddressHandler.playerAttemptLogin(event);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(final PlayerEvent.PlayerLoggedInEvent event) {
+        AddressHandler.playerLogin((ServerPlayer) event.getPlayer());
+
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedOut(final PlayerEvent.PlayerLoggedOutEvent event) {
+        AddressHandler.playerLogout((ServerPlayer) event.getPlayer());
     }
 
 }
