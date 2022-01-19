604057305d5d882fcf1a39074be18ed2df142664
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-19 13:26:19 +0800

    feat: register command

---
+++ src/main/java/org/auioc/mods/addrlimiter/server/command/ServerCommandRegistry.java
@@ -0,0 +1,20 @@
+package org.auioc.mods.addrlimiter.server.command;
+
+import static net.minecraft.commands.Commands.literal;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.CommandNode;
+import org.auioc.mods.addrlimiter.AddrLimiter;
+import org.auioc.mods.arnicalib.server.command.impl.VersionCommand;
+import net.minecraft.commands.CommandSourceStack;
+
+public final class ServerCommandRegistry {
+
+    public static final CommandNode<CommandSourceStack> NODE = literal(AddrLimiter.MOD_ID).build();
+
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        NODE.addChild(literal("version").executes((ctx) -> VersionCommand.getModVersion(ctx, AddrLimiter.MAIN_VERSION, AddrLimiter.FULL_VERSION, AddrLimiter.MOD_NAME)).build());
+
+        org.auioc.mods.arnicalib.server.command.ServerCommandRegistry.getRootNode(dispatcher).addChild(NODE);
+    }
+
+}
--- src/main/java/org/auioc/mods/addrlimiter/server/event/ServerEventHandler.java
+++ src/main/java/org/auioc/mods/addrlimiter/server/event/ServerEventHandler.java
@@ -1,5 +1,14 @@
 package org.auioc.mods.addrlimiter.server.event;
 
+import org.auioc.mods.addrlimiter.server.command.ServerCommandRegistry;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
 public final class ServerEventHandler {
+
+    @SubscribeEvent
+    public static void registerCommands(final RegisterCommandsEvent event) {
+        ServerCommandRegistry.register(event.getDispatcher());
+    }
 
 }
